7a6d40e3334e9f22e4138ce62b0b0a97355cf87a
Rayyan Merchant <merchantrayyan43@gmail.com>
2025-08-11 00:29:10 +0500

    Create mock.py

---
+++ src/utils/mock.py
@@ -0,0 +1,7 @@
+def load_mock_image():
+    """
+    Returns a placeholder image bytes object.
+    Can be extended to load from a local fixtures folder.
+    """
+    # Just return empty bytes for now in mock mode
+    return b"MOCK_IMAGE_DATA"
