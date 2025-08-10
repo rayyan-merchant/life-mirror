3d76546912fed6c77be07c3fc973c65ff0fe9e3c
Rayyan Merchant <merchantrayyan43@gmail.com>
2025-08-11 00:15:33 +0500

    Update celery_app.py

--- src/workers/celery_app.py
+++ src/workers/celery_app.py
@@ -1,1 +1,6 @@
+from celery import Celery
+import os
 
+BROKER_URL = os.getenv('REDIS_URL', 'redis://redis:6379/0')
+celery = Celery('lifemirror', broker=BROKER_URL)
+celery.conf.task_routes = {'src.workers.tasks.*': {'queue': 'media'}}
