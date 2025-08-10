1a3610038a530049220f9e145a844365e602b4af
Rayyan Merchant <merchantrayyan43@gmail.com>
2025-08-11 00:15:02 +0500

    Update session.py

--- src/db/session.py
+++ src/db/session.py
@@ -1,1 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
 
+DATABASE_URL = os.getenv('DATABASE_URL')
+engine = create_engine(DATABASE_URL, future=True)
+SessionLocal = sessionmaker(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
