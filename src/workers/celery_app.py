887376b807aa91ade87a2d2c368ce0a0b1bc0885
Rayyan Merchant <merchantrayyan43@gmail.com>
2025-08-13 17:46:56 +0500

    Update celery_app.py

--- src/workers/celery_app.py
+++ src/workers/celery_app.py
@@ -1,6 +1,16 @@
 from celery import Celery
 import os
+from celery.schedules import crontab
+
 
 BROKER_URL = os.getenv('REDIS_URL', 'redis://redis:6379/0')
 celery = Celery('lifemirror', broker=BROKER_URL)
 celery.conf.task_routes = {'src.workers.tasks.*': {'queue': 'media'}}
+
+celery_app.conf.beat_schedule = {
+    "check-notifications-every-6-hours": {
+        "task": "src.workers.tasks.check_notifications_async",
+        "schedule": crontab(minute=0, hour="*/6"),  # every 6 hours
+    },
+}
+
